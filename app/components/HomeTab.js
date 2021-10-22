881d0e1e8a9b393510f317b903e51ccac2742081
Harrison Rhodes <thr16@case.edu>
2021-10-22 13:34:13 -0400

    hometab update

---
+++ app/components/HomeTab.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function HomeTab(){
+    const name = "Richard";
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title1}>Hello {name}, Welcome to PrepPal! </Text>
+            <Text style={styles.title2}>Recent Recipes:</Text>
+            <Button style={styles.submitButton}
+            title="See All Saved Recipes"
+            color ="#E5101C"
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    title1: {
+        marginTop: 0,
+        paddingVertical: 6,
+        borderWidth: 3,
+        borderColor: "#20232a",
+        color: "black",
+        backgroundColor: "yellow",
+        fontSize: 20,
+        fontWeight: "bold",
+        borderRadius: 6,
+        textAlign: "center"
+    },
+    title2:{
+        marginTop: 6,
+        paddingVertical: 6,
+        borderWidth: 3,
+        borderColor: "#20232a",
+        color: "red",
+        backgroundColor: "purple",
+        fontSize: 30,
+        fontWeight: "bold",
+        borderRadius: 6,
+        textAlign: "center"
+    },
+    container: {
+      flex: 1,
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'top',
+    },
+    submitButton: {
+        position: 'absolute',
+        bottom:0,
+        left:0,
+    },
+});
