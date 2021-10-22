4f8e31aa0a8ef8345eac3cd3d2f18720b9ddaf43
Audrey Zhu <aaz25@case.edu>
2021-10-22 14:04:24 -0400

    basic User Guide Tab

---
+++ app/components/UserGuideTab.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { ScrollView, Text, Title } from 'react-navtive';
+
+export default function UserGuideTab() {
+  return (
+    <ScrollView>
+      <Title>How to Use This App</Title>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim praesent elementum facilisis leo vel. Sed turpis tincidunt id aliquet risus. Aliquet sagittis id consectetur purus ut. Convallis tellus id interdum velit. Tellus orci ac auctor augue mauris augue neque gravida. Fermentum leo vel orci porta non pulvinar neque laoreet suspendisse. Urna porttitor rhoncus dolor purus non enim praesent elementum. Risus in hendrerit gravida rutrum. At tempor commodo ullamcorper a lacus.
+      </Text>
+      <Text>
+        Sed odio morbi quis commodo odio aenean. Sed id semper risus in hendrerit gravida rutrum quisque. Non tellus orci ac auctor augue mauris augue neque. In dictum non consectetur a erat nam at lectus urna. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Magna fringilla urna porttitor rhoncus dolor. Vel fringilla est ullamcorper eget nulla facilisi etiam. Dictum varius duis at consectetur lorem donec massa sapien faucibus. Lectus null~a at volutpat diam ut venenatis tellus. Nulla facilisi cras fermentum odio eu. Consequat id porta nibh venenatis cras sed. Tellus at urna condimentum mattis pellentesque. Elementum eu facilisis sed odio morbi quis. Mattis enim ut tellus elementum sagittis vitae et leo. Ornare arcu dui vivamus arcu felis bibendum. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Nunc eget lorem dolor sed viverra ipsum nunc. In fermentum posuere urna nec. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Sed ullamcorper morbi tincidunt ornare massa.
+      </Text>
+      <Text>
+        Massa tincidunt dui ut ornare lectus sit amet est placerat. Sagittis aliquam malesuada bibendum arcu vitae. In cursus turpis massa tincidunt dui ut ornare lectus. Lectus urna duis convallis convallis. Mattis pellentesque id nibh tortor id aliquet lectus. Maecenas pharetra convallis posuere morbi leo urna molestie at elementum. Est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat. Sagittis purus sit amet volutpat. Tortor pretium viverra suspendisse potenti nullam ac tortor vitae. Urna duis convallis convallis tellus id interdum. Nunc congue nisi vitae suscipit tellus mauris a diam maecenas. Imperdiet massa tincidunt nunc pulvinar. Ultrices dui sapien eget mi proin sed libero. Pellentesque diam volutpat commodo sed egestas egestas fringilla. Auctor eu augue ut lectus. Diam maecenas sed enim ut sem viverra aliquet eget sit. Dolor sit amet consectetur adipiscing. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Vitae suscipit tellus mauris a.
+      </Text>
+    </ScrollView>
+  )
+};
