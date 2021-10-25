e4111ff522fc6e1998896b140d31b6103fd33294
Audrey Zhu <aaz25@case.edu>
2021-10-25 15:08:53 -0400

    Framework for unit tests

--- app/components/SearchTab.js
+++ app/components/SearchTab.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import { Chip, TextInput } from 'react-native-paper';
 
 function SearchTab() {
+
+  const [ingredients, setIngredients] = useState([]);
+
   return (
+
     <View style={styles.container}>
-      <Text>This is the Search Tab</Text> 
+      <Text style={styles.title}>Search for Recipes</Text>
+      <TextInput
+        label="Enter an ingredient"
+        value={ingredients}
+        onChangeText={text => setIngredients(ingredients.concat(text))}
+      />
+      <Surface style={styles.box}>
+        {ingredients.map(ingredient => (
+          <Chip onClose={() => {
+            var index = ingredients.indexOf(ingredient);
+            if (index > -1) {
+              ingredients.splice(index, 1);
+            }
+            setIngredients(ingredients);
+          }}
+          >
+            {ingredient}
+          </Chip>
+        ))}
+      </Surface>
+
     </View>
   )
 }
@@ -13,7 +37,21 @@ function SearchTab() {
 const styles = StyleSheet.create({
   container: {
     alignItems: "center"
+  },
+  box: {
+    flexDirection: "row",
+    padding: 8,
+    height: 80,
+    width: 80,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    elevation: 0,
+    color: '#fafafa'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold"
   }
-})
+});
 
 export default SearchTab;
