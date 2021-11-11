2a5168c99678b9e81b4029d1bf59a2cc96e97c62
Audrey Zhu <aaz25@case.edu>
2021-11-11 10:33:17 -0500

    :/

--- app/_test_/Container.test.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import renderer from 'react-test-renderer';
-import Container from /../components/Container.js;
--- app/_test_/SavedRecipesTab.test.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import renderer from 'react-test-renderer';
-import SavedRecipesTab from /../components/SavedRecipesTab.js;
