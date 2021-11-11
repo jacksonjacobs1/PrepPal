a513ca866d19cf4bd567bbfe388293eb8506fd81
Audrey Zhu <aaz25@case.edu>
2021-11-10 19:52:40 -0500

    More testing shenanigans

--- app/__tests__/Container.test.js
+++ app/__tests__/Container.test.js
@@ -1,9 +1,11 @@
 import React from "react";
 import renderer from 'react-test-renderer';
 import { ReactDOM } from "react";
-import Container from /../components/Container.js;
+import Container from '../components/Container.js';
 
 it('renders correctly', () => {
   const tree = renderer.create(<Container />).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+
