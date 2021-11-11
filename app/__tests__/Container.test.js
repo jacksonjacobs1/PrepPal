3e4c113bfad7bab2281c096b0857c72b5f9453a5
Audrey Zhu <aaz25@case.edu>
2021-11-11 12:27:42 -0500

    First functional requirement for Container

--- app/__tests__/Container.test.js
+++ app/__tests__/Container.test.js
@@ -1,11 +1,49 @@
 import React from "react";
 import renderer from 'react-test-renderer';
+import { cleanup, render, fireEvent } from '@testing-library/react-native';
 import { ReactDOM } from "react";
 import Container from '../components/Container.js';
 
-it('renders correctly', () => {
-  const tree = renderer.create(<Container />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+describe('Container', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Container />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
+  it('displays the navbar on the home tab', () => {
+    const testIdName = 'home';
+    const { getByTestId } = render(<Container />);
+    const button = getByTestId(testIdName);
+    fireEvent.press(button);
+    const foundButton = getByTestId(testIdName);
+    expect(foundButton).toBeTruthy();
+  });
+
+  it('displays the navbar on the saved recipes tab', () => {
+    const testIdName = 'list-outline';
+    const { getByTestId } = render(<Container />);
+    const button = getByTestId(testIdName);
+    fireEvent.press(button);
+    const foundButton = getByTestId('list');
+    expect(foundButton).toBeTruthy();
+  });
+
+  it('displays the navbar on the search tab', () => {
+    const testIdName = 'search-outline';
+    const { getByTestId } = render(<Container />);
+    const button = getByTestId(testIdName);
+    fireEvent.press(button);
+    const foundButton = getByTestId('search');
+    expect(foundButton).toBeTruthy();
+  });
+
+  it('displays the navbar on the user guide tab', () => {
+    const testIdName = 'book-outline';
+    const { getByTestId } = render(<Container />);
+    const button = getByTestId(testIdName);
+    fireEvent.press(button);
+    const foundButton = getByTestId('book');
+    expect(foundButton).toBeTruthy();
+  });
+});
 
--- app/__tests__/ProfileTab.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from 'react-test-renderer';
-import ProfileTab from '../components/ProfileTab';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<ProfileTab />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
